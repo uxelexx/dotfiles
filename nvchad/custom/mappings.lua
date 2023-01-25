e8485e20de2539634841fdea90e24de56043abb1
uxelexx <i@kkornilov.ru>
2023-01-25 13:35:08 +0300

    init

---
+++ nvchad/custom/mappings.lua
@@ -0,0 +1,61 @@
+local M = {
+
+}
+
+M.general = {
+  n = {
+    ["H"] = { "^", "line start" },
+    ["L"] = { "$", "line end" },
+
+    -- toggle ZenMode
+    ["<leader>zz"] = { ":ZenMode <CR>", "toggle ZenMode" },
+
+    -- center while search next
+    ["n"] = {"nzzzn", "center"},
+    ["N"] = {"Nzzzn", "center"},
+
+    -- select all
+    ["<C-a>"] = { "gg<s-v>G", "Select all" },
+
+    -- center while seach 
+    ["<C-d>"] = { "<C-d>zz" },
+    ["<C-u>"] = { "<C-u>zz" },
+
+
+  },
+
+  v = {
+    --move lines
+    ["K"] = { ":m '<-2<CR>gv=gv", "move line up" },
+    ["J"] = { ":m '>+1<CR>gv=gv", "move line down" },
+
+    ["H"] = {"^", "line start"},
+    ["L"] = {"$", "line end"},
+
+    -- yank to clipboard
+    ["<leader>c"] = { '"*y', "yank to clipboard"},
+
+  },
+
+  i = {
+    -- exit insert mode
+    ["jj"] = { "<ESC>", "Escape insert mode" },
+
+      -- function()
+      --   vim.lsp.buf.format { async = true }
+      -- end,
+
+    -- save and exit insert mode
+    ["<C-s>"] = { "<cmd> w <CR><ESC>", "save file and escape" },
+  }
+}
+
+M.telescope = {
+  plugin = true,
+
+  n = {
+    -- find
+    ["<leader>ff"] = { "<cmd> Telescope find_files <CR>", "find files" },
+  }
+}
+return M
