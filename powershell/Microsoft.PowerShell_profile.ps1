faeef1ee4fa9e0e8fa5da7399fe54fe72ef84a13
uxelexx <i@kkornilov.ru>
2024-01-03 11:20:46 +0300

    fixes

--- powershell/Microsoft.PowerShell_profile.ps1
+++ powershell/Microsoft.PowerShell_profile.ps1
@@ -1,7 +1,7 @@
-[console]::InputEncoding = [console]::OutputEncoding = New-Object System.Text.UTF8Encoding
+# $omp_config = Join-Path $PSScriptRoot '.\uxelexx.omp.json'
+# oh-my-posh --init --shell pwsh --config $omp_config | Invoke-Expression
 
-$omp_config = Join-Path $PSScriptRoot '.\uxelexx.omp.json'
-oh-my-posh --init --shell pwsh --config $omp_config | Invoke-Expression
+oh-my-posh --init --shell pwsh --config 'C:\Users\Kirill\AppData\Local\Programs\oh-my-posh\themes\uxelexx.omp.json' | Invoke-Expression
 
 ######### PSReadLine
 Set-PSReadLineOption -EditMode Emacs
@@ -11,14 +11,12 @@ Set-PSReadLineOption -PredictionSource History
 Set-PSReadLineOption -PredictionViewStyle ListView
 
 ######### Fzf
-Import-Module PSFzf
 Set-PSFzfOption -PSReadLineChordProvider 'Ctrl+f' -PSReadLineChordReverseHitory 'Ctrl+r'
 
 ######### Allias
 Set-Alias g git
 Set-Alias lg lazygit
 Set-Alias ex explorer
-# Set-Alias less 'C:\Program Files\Git\usr\bin\less.exe'
 Set-Alias vi nvim
 
 
