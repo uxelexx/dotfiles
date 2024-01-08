841599b82f18999c8160b59beb6eac962e8487a0
uxelexx <i@kkornilov.ru>
2024-01-08 22:31:24 +0300

    little refactor, trying to use codeium with cmp

--- nvchad/custom/plugins.lua
+++ nvchad/custom/plugins.lua
@@ -1,46 +1,23 @@
 local overrides = require "custom.configs.overrides"
 
 --@type NvPluginSpec[]
-
 return {
   {
-    "hrsh7th/nvim-cmp",
-    opts = overrides.cmp,
-
-    dependencies = {
-      {
-        -- snippet plugin
-        "L3MON4D3/LuaSnip",
-        config = function(_, opts)
-          -- load default luasnip config
-          require("plugins.configs.others").luasnip(opts)
-
-          -- local luasnip = require "luasnip"
-          -- luasnip.filetype_extend("javascriptreact", { "html" })
-          -- luasnip.filetype_extend("typescriptreact", { "html" })
-          require("luasnip/loaders/from_vscode").lazy_load()
-        end,
-      },
-
-      -- ai based completion
-      {
-        "Exafunction/codeium.nvim",
-        config = function()
-          require("codeium").setup()
-        end,
-      },
-    },
-  },
-
-  {
     "neovim/nvim-lspconfig",
-    dependencies = {},
+    -- dependencies = {},
 
     config = function()
       require "plugins.configs.lspconfig"
       require "custom.configs.lspconfig"
     end,
   },
+
+  { "nvim-tree/nvim-tree.lua", opts = overrides.nvimtree },
+  { "nvim-treesitter/nvim-treesitter", opts = overrides.treesitter },
+  { "nvim-tree/nvim-tree.lua", opts = overrides.nvimtree },
+  { "williamboman/mason.nvim", opts = overrides.mason },
+
+  ---formatting
 
   {
     "stevearc/conform.nvim",
@@ -49,22 +26,9 @@ return {
     end,
   },
 
-  {
-    "NvChad/nvim-colorizer.lua",
-    config = function()
-      require("colorizer").setup {
-        user_default_options = {
-          hsl_fn = true, -- CSS hsl() and hsla() functions
-        },
-      }
-    end,
-  },
-
-  { "nvim-tree/nvim-tree.lua", opts = overrides.nvimtree },
-  { "nvim-treesitter/nvim-treesitter", opts = overrides.treesitter },
-  { "williamboman/mason.nvim", opts = overrides.mason },
-
   -------------------------- custom plugins -----------------------------
+
+  --- quality of life
 
   {
     "windwp/nvim-ts-autotag",
@@ -77,6 +41,7 @@ return {
   {
     "kylechui/nvim-surround",
     version = "*", -- Use for stability; omit to use `main` branch for the latest features
+    event = "InsertEnter",
     config = function()
       require("nvim-surround").setup {
         -- Configuration here, or leave empty to use defaults
@@ -90,14 +55,6 @@ return {
   },
 
   {
-    "folke/zen-mode.nvim",
-    cmd = "ZenMode",
-    config = function()
-      require "custom.configs.zenmode"
-    end,
-  },
-
-  {
     "Pocco81/auto-save.nvim",
     event = "InsertEnter",
     config = function()
@@ -108,8 +65,9 @@ return {
   {
     "nvim-treesitter/nvim-treesitter-context",
     event = "InsertEnter",
-    dependencies = "nvim-treesitter",
-    opts = { max_lines = 2 },
+    opts = {
+      max_lines = 2,
+    },
   },
 
   {
@@ -118,40 +76,75 @@ return {
     config = function()
       require "custom.configs.lspsaga"
     end,
-    dependencies = {
-      "nvim-treesitter/nvim-treesitter", -- optional
-      "nvim-tree/nvim-web-devicons", -- optional
-    },
   },
 
   {
     "folke/trouble.nvim",
     cmd = "Trouble",
-    dependencies = { "nvim-tree/nvim-web-devicons" },
     config = function()
       require("trouble").setup()
     end,
   },
-}
 
--- Old codeium
---[[
-{
-"Exafunction/codeium.nvim",
-event = "BufEnter",
-config = function()
-  require("codeium").setup()
-end,
-config = function()
-  vim.keymap.set("i", "<C-y>", function()
-    return vim.fn["codeium#Accept"]()
-  end, { expr = true })
-  vim.keymap.set("i", "<C-]>", function()
-    return vim.fn["codeium#CycleCompletions"](1)
-  end, { expr = true })
-  vim.keymap.set("i", "<C-.>", function()
-    return vim.fn["codeium#CycleCompletions"](-1)
-  end, { expr = true })
-end,
-}, 
-]]
+  {
+    "hrsh7th/nvim-cmp",
+    opts = overrides.cmp,
+
+    dependencies = {
+      {
+        -- snippet plugin
+        "L3MON4D3/LuaSnip",
+        config = function(_, opts)
+          -- load default luasnip config
+          require("plugins.configs.others").luasnip(opts)
+
+          local luasnip = require "luasnip"
+          luasnip.filetype_extend("javascriptreact", { "html" })
+          luasnip.filetype_extend("typescriptreact", { "html" })
+          require("luasnip/loaders/from_vscode").lazy_load()
+        end,
+      },
+      {
+        "Exafunction/codeium.nvim",
+        config = function()
+          require("codeium").setup()
+        end,
+      },
+      {
+        "Exafunction/codeium.vim",
+        -- lazy = "true",
+        config = function()
+          vim.keymap.set("i", "<C-y>", function()
+            return vim.fn["codeium#Accept"]()
+          end, { expr = true, silent = true })
+          vim.keymap.set("i", "<C-]>", function()
+            return vim.fn["codeium#CycleCompletions"](1)
+          end, { expr = true, silent = true })
+          vim.keymap.set("i", "<C-.>", function()
+            return vim.fn["codeium#CycleCompletions"](-1)
+          end, { expr = true, silent = true })
+        end,
+      },
+    },
+  },
+
+  --- ui stuff
+  {
+    "folke/zen-mode.nvim",
+    cmd = "ZenMode",
+    config = function()
+      require "custom.configs.zenmode"
+    end,
+  },
+
+  {
+    "NvChad/nvim-colorizer.lua",
+    config = function()
+      require("colorizer").setup {
+        user_default_options = {
+          hsl_fn = true, -- CSS hsl() and hsla() functions
+        },
+      }
+    end,
+  },
+}
